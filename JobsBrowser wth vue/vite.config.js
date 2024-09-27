f30153d44d14b02efbf6e637b398d5ae2e29bd9c
mahmoud-ramadn <mr9026573@gmail.com>
2024-09-27 04:35:27 +0300

    s

--- JobsBrowser wth vue/vite.config.js
+++ JobsBrowser wth vue/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target:'http://localhost:8000',
+        target:'https://vue-project-kka9.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
