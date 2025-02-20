959d232e7a4c1dc7c8454df4bc8c1aecda09d6ef
mahmoud-ramadn <mr9026573@gmail.com>
2025-02-20 18:40:58 +0200

    chore: fix backEndPath

--- JobsBrowser wth vue/vite.config.js
+++ JobsBrowser wth vue/vite.config.js
@@ -1,22 +1,26 @@
-const isProduction = process.env.NODE_ENV === "production";
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+
+  plugins: [vue(),],
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: isProduction
-          ? "https://vue-project-kka9.vercel.app/"
-          : "http://localhost:3000",
+      '/api': {
+        target:'https://vue-project-kka9.vercel.app/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+  
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
